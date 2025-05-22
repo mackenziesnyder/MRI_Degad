c2dd4c5b41caf435d57096f4502f56fc0d4ab8a1
mackenziesnyder <masnyder2@hotmail.com>
2025-05-22 15:16:08 -0400

    add code for read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,20 +6,23 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import datetime
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
 
-project = "mri_degad"
-copyright = "2025, mackenziesnyder"
-author = "mackenziesnyder"
+project = "MRI_Degad"
+copyright = f"{datetime.date.today().year}, Mackenzie Snyder"
+author = "Mackenzie Snyder"
 
 
 # -- General configuration ---------------------------------------------------
@@ -55,4 +58,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
